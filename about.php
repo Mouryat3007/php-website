bf72bf72c3cc6498ae0df2bc11ab87c2c3cce595
Mouryat3007 <92813285+Mouryat3007@users.noreply.github.com>
2023-11-01 21:02:36 +0000

    Create about.php

---
+++ about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us</title>
+</head>
+<body>
+    <header>
+        <h1>About Us</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <section>
+            <h2>About Us</h2>
+            <p>This is the about page of my website.</p>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> My Website</p>
+    </footer>
+</body>
+</html>
